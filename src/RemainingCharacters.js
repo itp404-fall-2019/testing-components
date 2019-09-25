d3151f86650e56e9bdf2a54b4af99d333720ed3c
David Tang <dtang859@gmail.com>
2019-09-24 19:02:41 -0700

    adds propTypes to RemainingCharacters component

--- src/RemainingCharacters.js
+++ src/RemainingCharacters.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default function RemainingCharacters(props) {
   const { max, text, children } = props;
@@ -12,3 +13,8 @@ export default function RemainingCharacters(props) {
     );
   }
 }
+
+RemainingCharacters.propTypes = {
+  text: PropTypes.string.isRequired,
+  max: PropTypes.number.isRequired
+};
